0196269e398163c9d9c049daa2829226284353c6
Shashikant-15 <shashikant.tanti@knoldus.com>
2023-07-12 16:42:44 +0530

    NetworkDataSource.kt updated

--- src/main/kotlin/com/knoldus/kotlinrestapi/datasource/network/NetworkDataSource.kt
+++ src/main/kotlin/com/knoldus/kotlinrestapi/datasource/network/NetworkDataSource.kt
@@ -4,6 +4,7 @@ import com.knoldus.kotlinrestapi.datasource.BankDataSource
 import com.knoldus.kotlinrestapi.datasource.network.dto.BankList
 import com.knoldus.kotlinrestapi.model.Bank
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Repository
 import org.springframework.web.client.RestTemplate
@@ -12,39 +13,38 @@ import java.io.IOException
 
 @Repository("network")
 class NetworkDataSource(
-    @Autowired private val restTemplate: RestTemplate
+        @Autowired private val restTemplate: RestTemplate,
+        @Value("http://54.193.31.159/banks") private val bankApiUrl: String
 ) : BankDataSource {
 
     override fun getBanks(): Collection<Bank> {
-
-       val response: ResponseEntity<BankList> =
-           restTemplate.getForEntity<BankList>("http://54.193.31.159/banks")
-       return  response.body?.results
-            ?: throw IOException(" Could not fetch banks from the network")
+        val response: ResponseEntity<BankList> =
+                restTemplate.getForEntity<BankList>("$bankApiUrl/banks")
+        return response.body?.results
+                ?: throw IOException("Could not fetch banks from the network")
     }
 
     override fun retrieveBank(accountNum: String): Bank {
         val response: ResponseEntity<Bank> =
-                restTemplate.getForEntity("http://54.193.31.159/banks/$accountNum", Bank::class.java)
+                restTemplate.getForEntity("$bankApiUrl/banks/$accountNum", Bank::class.java)
         return response.body
                 ?: throw IOException("Could not retrieve bank with account number $accountNum from the network")
     }
 
     override fun createBank(bank: Bank): Bank {
-        restTemplate.postForObject<Unit>("http://54.193.31.159/banks", bank, Unit::class.java)
+        restTemplate.postForObject<Unit>("$bankApiUrl/banks", bank, Unit::class.java)
         return bank
     }
 
     override fun updateBank(bank: Bank): Bank {
-        restTemplate.put("http://54.193.31.159/banks/${bank.accountNum}", bank)
+        restTemplate.put("$bankApiUrl/banks/${bank.accountNum}", bank)
         return bank
     }
+
     override fun deleteBank(accountNum: String) {
-        restTemplate.delete("http://54.193.31.159/banks/$accountNum")
+        restTemplate.delete("$bankApiUrl/banks/$accountNum")
     }
 }
-
-
 
 
 
